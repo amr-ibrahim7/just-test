db97b6f0989e2ab47c0fe99b62b8163ed34cc2ac
amr-ibrahim7 <amrnew10@gmail.com>
2025-08-21 07:52:49 +0300

    feat: integrate Tailwind CSS for styling

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,5 +1,12 @@
 <script setup></script>
 
 <template>
-  <h2>Hello f world</h2>
+  <div class="p-8 bg-gray-100 min-h-screen">
+    <h1 class="text-3xl font-bold text-blue-600 underline">Movie Rating App</h1>
+    <button
+      class="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-700 transition-colors"
+    >
+      Add Movie
+    </button>
+  </div>
 </template>
